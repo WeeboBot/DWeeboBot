ad72481325b1fd287bbc303b4e43016a958a38c5
James Wolff <james.wolff0@gmail.com>
2015-08-16 15:08:16 -0500

    Revert "Development"

---
+++ src/me/mage/bot/commands/TopExemption.java
@@ -0,0 +1,37 @@
+package me.mage.bot.commands;
+
+import me.mage.bot.database.Database;
+import me.mage.bot.util.CLevel;
+
+public class TopExemption extends Command {
+
+	@Override
+	public CLevel getCommandLevel() {
+		return CLevel.Mod;
+	}
+
+	@Override
+	public String getCommandText() {
+		return "topexemption";
+	}
+
+	@Override
+	public String execute(String channel, String sender, String... parameters) {
+		if (parameters.length == 2) {
+			if (!parameters[0].toLowerCase().matches("(add|remove)")) {
+				return "Incorrect parameters. Correct usage is !topexemption <add|remove> <username>";
+			}
+			if (parameters[0].toLowerCase().equals("add")) {
+				Database.topExemption(channel.substring(1), parameters[1], false);
+				return String.format("%s was successfully added to the list of people exempt from the !top command!",
+						parameters[1]);
+			}
+			Database.topExemption(channel.substring(1), parameters[1], true);
+			return String.format("%s was successfully removed from the list of people exempt from the !top command!",
+					parameters[1]);
+		} else {
+			return "Insufficient paramaters. Format as !topexemption <add|remove> <username>";
+		}
+	}
+
+}
