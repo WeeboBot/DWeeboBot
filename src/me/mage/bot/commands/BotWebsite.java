bd8a35369dfa898e956361d370b61d17a96e9cab
MysteriousMage <timothy@riftware.com>
2015-08-18 22:49:23 -0500

    Bot website and bot reddit commands

---
+++ src/me/mage/bot/commands/BotWebsite.java
@@ -0,0 +1,22 @@
+package me.mage.bot.commands;
+
+import me.mage.bot.util.CLevel;
+
+public class BotWebsite extends Command{
+
+	@Override
+	public CLevel getCommandLevel() {
+		return CLevel.Normal;
+	}
+
+	@Override
+	public String getCommandText() {
+		return "botwebsite";
+	}
+
+	@Override
+	public String execute(String channel, String sender, String... parameters) {
+		return "The website for WeeboBot can be found here http://weebobot.no-ip.info/";
+	}
+
+}
